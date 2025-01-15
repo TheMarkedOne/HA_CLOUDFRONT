6b8f7966a4892ed9edaa626e0ae7cf9dde77b1a7
zura <zura.badzagua@makingscience.com>
2025-01-15 14:32:09 +0400

    finish

--- modules/lambda/lambda_failover.py
+++ modules/lambda/lambda_failover.py
@@ -1,7 +1,6 @@
 import boto3
 import os
 
-# Retrieve environment variables
 EIP = os.environ['EIP']
 ACTIVE_INSTANCE = os.environ['ACTIVE_INSTANCE']
 PASSIVE_INSTANCE = os.environ['PASSIVE_INSTANCE']
@@ -10,7 +9,6 @@ def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
 
     try:
-        # Get current EIP association
         addresses = ec2.describe_addresses(PublicIps=[EIP])
         allocation_id = addresses['Addresses'][0]['AllocationId']
         current_instance = addresses['Addresses'][0].get('InstanceId', None)
@@ -19,7 +17,6 @@ def lambda_handler(event, context):
         print(f"Active instance: {ACTIVE_INSTANCE}")
         print(f"Passive instance: {PASSIVE_INSTANCE}")
 
-        # Check the state of the currently associated instance (if any)
         if current_instance:
             instance_response = ec2.describe_instances(InstanceIds=[current_instance])
             current_instance_state = instance_response['Reservations'][0]['Instances'][0]['State']['Name']
@@ -27,12 +24,10 @@ def lambda_handler(event, context):
         else:
             current_instance_state = None
 
-        # Check the state of the active instance
         active_instance_response = ec2.describe_instances(InstanceIds=[ACTIVE_INSTANCE])
         active_instance_state = active_instance_response['Reservations'][0]['Instances'][0]['State']['Name']
         print(f"Active instance state: {active_instance_state}")
 
-        # Failover logic
         if active_instance_state != "running" or current_instance_state != "running":
             print("Active instance is not running or EIP is not correctly associated. Triggering failover.")
             ec2.associate_address(
